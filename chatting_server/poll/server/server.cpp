8790bb0a7d30ee4736c4d9da8a09cdb714597dc5
ktj4060 <ktj4060@naver.com>
2025-05-07 10:16:05 +0900

    add

--- chatting_server/poll/server/server.cpp
+++ chatting_server/poll/server/server.cpp
@@ -5,6 +5,7 @@
 #include <netinet/in.h>
 #include <vector>
 #include <termios.h>
+#include <poll.h>
 
 #define BUFFER_SIZE 1024
 
@@ -26,8 +27,6 @@ int main(int argc, char *argv[]) {
     int addrlen = sizeof(address);
     char buffer[BUFFER_SIZE] = {0};
     Packet packet;
-    fd_set read_fds;
-    FD_ZERO(&read_fds);
 
     // 1. 서버 소켓 생성
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -60,25 +59,24 @@ int main(int argc, char *argv[]) {
     std::cout << "Server listening on port " << port << "..." << std::endl;
 
     std::vector<int> client_fds;
-    int fd_max = server_fd, fd_num;
+    
+    struct pollfd fds[1024];
+    fds[0].fd = server_fd;
+    fds[0].events = POLLIN;
+    for(int i = 1; i < 1024; i++) {
+        fds[i].fd = -1; // 초기화
+    }
 
     while(1) {
-        //fd_set 초기화 및 소켓 추가
-        fd_set temp_fds;
-        FD_ZERO(&temp_fds);
-        FD_SET(server_fd, &temp_fds);
-        for (int client_fd : client_fds) {
-            FD_SET(client_fd, &temp_fds);
-        }
 
-        // select 호출
-        if((fd_num = select(fd_max + 1, &temp_fds, NULL, NULL, NULL)) < 0){
-            perror("select failed");
+        int activity = poll(fds, client_fds.size() + 1, -1);
+        if (activity < 0) {
+            perror("poll error");
             exit(EXIT_FAILURE);
         }
 
         // 새로운 클라이언트 접속 확인
-        if (FD_ISSET(server_fd, &temp_fds)) {
+        if (fds[0].revents & POLLIN) {
             int temp_client = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
             if (temp_client < 0) {
                 perror("accept failed");
@@ -87,29 +85,26 @@ int main(int argc, char *argv[]) {
 
             client_fds.push_back(temp_client);
 
-            if (temp_client > fd_max) {
-                fd_max = temp_client;
-            }
+            fds[client_fds.size()].fd = temp_client;
+            fds[client_fds.size()].events = POLLIN;
 
             std::cout << client_fds.size() << " Client connected!" << std::endl;
         }
 
         // 기존 클라이언트들 데이터 읽기 및 에코 처리
-        for (auto it = client_fds.begin(); it != client_fds.end(); ) {
-            if (FD_ISSET(*it, &temp_fds)) {
-                int n = read(*it, &packet, sizeof(packet));
+        for(int i = 1; i < client_fds.size() + 1; i++) {
+            if (fds[i].revents & POLLIN) {
+                int n = read(fds[i].fd, &packet, sizeof(packet));
                 if (n <= 0) {
-                    close(*it);
-                    it = client_fds.erase(it);
+                    close(fds[i].fd);
+                    fds[i].fd = -1;
+                    client_fds.erase(client_fds.begin() + (i - 1));
                     std::cout << "Client left, "<< client_fds.size() << " Client left" << std::endl;
                 } else {
                     for (auto iter = client_fds.begin(); iter != client_fds.end(); iter++) 
-                        if(*it != *iter) write(*iter, &packet, sizeof(packet));
-                    ++it;
+                        if(*iter != fds[i].fd) write(*iter, &packet, sizeof(packet));
                 }
-            } else {
-                ++it;
-            }
+            } 
         }
     }
 
